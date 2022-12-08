376127bd8df993acc8ad2d96c9cc4f572c45d83a
saidgadgiev <117773666+saidgadgiev@users.noreply.github.com>
2022-12-08 23:56:31 +0300

    add task 19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное 
+число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.WriteLine("Введите пятизначное число -> ");
+// string num = Console.ReadLine()??"";
+string num = "13321";
+void Polidrom(string num)
+{
+    int lenght = num.Length;
+    int lenghtEnd = lenght - 1;
+    int lenPol = lenght / 2;
+    int count = 0;
+
+    for (int i = 0; i < lenPol; i++)
+    {
+        if (num[i] == num[lenghtEnd])
+        {
+            count++;
+            lenghtEnd--;
+            
+        } 
+    }
+    // Console.WriteLine(count);
+    if (count == lenPol) Console.WriteLine($"Число {num} являеться полидромом");
+    else Console.WriteLine($"Число {num} не являеться полидромом");
+}
+
+Polidrom(num);
