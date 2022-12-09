aa930492f0f60f6cc23a52fd0dc6f7dc634e669e
saidgadgiev <117773666+saidgadgiev@users.noreply.github.com>
2022-12-09 13:33:39 +0300

    finish task 23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -5,3 +5,32 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+int CorrectNumber(string messege)
+{
+    bool isCorrect = false;
+    int num = 0;
+    while(!isCorrect)
+    {
+        Console.Write(messege);
+        if(int.TryParse(Console.ReadLine(), out num))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное число.");
+        }
+    }
+    return Math.Abs(num);
+}
+void Desition (int n)
+{
+    for (int i = 1; i < n+1; i++)
+    {
+        double result = Math.Pow(i,3);
+        Console.Write($"{result} ");
+    }
+}
+
+int num = CorrectNumber("Введите число -> ");
+Desition(num);
